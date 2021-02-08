aef551978f79cef66181e03f610fe83cc9438eb1
Aarti Bhagtani <bhagtaniaarti@gmail.com>
2021-02-08 22:07:35 +0530

    add pagination

--- app.js
+++ app.js
@@ -97,6 +97,7 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
+  console.log(error);
   // res.status(error.httpStatusCode).render(...)
   res.status(500).render('500', {
     pageTitle: 'Error',
--- controllers/shop.js
+++ controllers/shop.js
@@ -4,13 +4,30 @@ const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
-  Product.find()
+let totalItems;
+  const page = +req.query.page || 1;
+  Product.find().countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  })
   .then(products => {
-    res.render('shop/product-list', {
-      prods: products, 
-      pageTitle: 'Shop', 
-      path: '/products'
+    res.render('shop/product-list',{
+        prods: products, 
+        pageTitle: 'Shop', 
+        path: '/products',
+        csrfToken: req.csrfToken(),
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        previousPage: page - 1,
+        nextPage: page + 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
     })
   })
   .catch(err => {
@@ -18,7 +35,21 @@ exports.getProducts = (req, res, next) => {
     const error = new Error(err);
     error.httpStatusCode = 500;
     return next(error);
-  });  
+  });
+  // Product.find()
+  // .then(products => {
+  //   res.render('shop/product-list', {
+  //     prods: products, 
+  //     pageTitle: 'Shop', 
+  //     path: '/products'
+  //   })
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  //   const error = new Error(err);
+  //   error.httpStatusCode = 500;
+  //   return next(error);
+  // });  
 };  
 
 exports.getProduct = (req, res, next) => {
@@ -42,13 +73,27 @@ exports.getProduct = (req, res, next) => {
 };
  
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  let totalItems;
+  const page = +req.query.page || 1;
+  Product.find().countDocuments()
+  .then(numProducts => {
+    totalItems = numProducts;
+    return Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
+  })
   .then(products => {
     res.render('shop/index',{
         prods: products, 
         pageTitle: 'Index', 
         path: '/',
-        csrfToken: req.csrfToken()
+        csrfToken: req.csrfToken(),
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        previousPage: page - 1,
+        nextPage: page + 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       })
   })
   .catch(err => {
